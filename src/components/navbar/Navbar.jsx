31c46a807ca81938caafeccd21329733c22956dc
r.dejesus <r.dejesus@lionheart.asia>
2023-09-22 08:28:40 +0800

    added new files

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+  return (
+    <div className="c-navbar">
+        <div className="container">
+            <h1 className="c-navbar__hdg">hello world</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
